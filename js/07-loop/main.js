42b4910fc5ed5fa2242e6b72ff82551ba91e931f
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-09-18 15:57:39 +0900

    add loop learning

--- js/07-loop/main.js
+++ js/07-loop/main.js
@@ -1,7 +1,25 @@
+// 반복문
+
 for (let number = 0; number < 3; number++) {
   console.log(number);
 }
 
-for (let i = 1; i < 5; i++){
-  
+for (let i = 1; i < 5; i++) {
+  console.log(i);
 }
+
+// while 반복문
+
+let j = 0;
+while (j <= 10) {
+  j++;
+  console.log(i);
+}
+
+// 의도적인 무한반복 가장 흔한 형태
+//while (true) { }
+
+// do...while : 코드를 무조건 한 번은 실행한다
+do {
+  console.log(k);
+} while (k > 1);
