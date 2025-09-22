fa2061cf9e3c3fb085959ef7613d38c1af5917da
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-09-22 11:47:44 +0900

    create object learning

---
+++ js/10-object/main.js
@@ -0,0 +1,24 @@
+// 객체
+// 0개 이상의 속성을 저장하는 자료형
+// 속성 = key(이름) + value(값)
+// {} 중괄호로 생성
+
+// 변수 emptyObject를 선언하고
+// 빈 객체를 할당
+let emptyObject = {};
+
+// 객체 데이터.key
+
+// 변수 person2 를 선언하고
+// 객체 데이터 할당
+// {
+//   name: "홍길동",
+//   score : 99,
+//   pass : true;
+// }
+
+let person2 = { name: "홍길동", score: 99, pass: true };
+// 객체 데이터 person2 에서
+// score key(이름)에 접근해서 value(값)을 출력
+console.log(person2["score"]);
+console.log(person2.score);
