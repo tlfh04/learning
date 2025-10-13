459d9694078ef83f3fe8d78083339a265b1da688
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-10-13 15:04:40 +0900

    Create ProtectedLayout learning

---
+++ react/react-router-app/src/layout/ProtectedLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+export default function ProtectedLayout() {
+  const isLogin = false;
+
+  if (!isLogin) {
+    <Navigate to="/auth/login"></Navigate>;
+  }
+  return <Outlet></Outlet>;
+}
