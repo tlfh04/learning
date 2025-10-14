4036e6bef26bcff57853fa249adeb6c146f8836e
tlfh04 <126224780+tlfh04@users.noreply.github.com>
2025-10-14 15:47:29 +0900

    Create NotFound learning

---
+++ react/react-router-app/src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div>페이지를 찾을 수 없습니다 404 error</div>;
+}
--- react/react-router-app/src/router/index.js
+++ react/react-router-app/src/router/index.js
@@ -2,8 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 
 import rootRoutes from "./routes/rootRoutes";
 import authRoute from "./routes/authRoutes";
+import NotFound from "../pages/NotFound";
 
 // 라우터 설정 생성
-const router = createBrowserRouter([...rootRoutes, ...authRoute]);
+const router = createBrowserRouter([
+  ...rootRoutes,
+  ...authRoute,
+  {
+    path: "*",
+    Component: NotFound,
+  },
+]);
 
 export default router;
