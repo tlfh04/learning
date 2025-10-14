881aa462f160473faf0c8773b2a603782b4463e0
yongmin Lee <126224780+tlfh04@users.noreply.github.com>
2025-10-14 10:48:21 +0900

    Edit quary-parameter learning (#4)

--- react/react-router-app/src/pages/RootPages/PostList.jsx
+++ react/react-router-app/src/pages/RootPages/PostList.jsx
@@ -2,19 +2,45 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import PostDetail from "./PostDetail";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+
 export default function PostList() {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
+    const order = searchParams.get("order") ?? "id";
+    const sortBy = searchParams.get("sortBy") ?? "asc";
+
     async function fetchData() {
-      const res = await axios.get(`https://dummyjson.com/posts`);
+      const res = await axios.get(
+        `https://dummyjson.com/posts?sortBy=${sortBy}&order=${order}`
+      );
       const data = res["data"]["posts"];
       setPosts(data);
     }
     fetchData();
-  }, []);
+  }, [searchParams]);
   return (
     <div>
+      <div className="flex gap-2">
+        <button
+          className="border-2 p-2 cursor-pointer"
+          onClick={() => {
+            setSearchParams({ sortBy: "id", order: "asc" });
+          }}
+        >
+          ID 오름차순
+        </button>
+        <button
+          className="border-2 p-2 cursor-pointer"
+          onClick={() => {
+            setSearchParams({ sortBy: "id", order: "desc" });
+          }}
+        >
+          ID 내림차순
+        </button>
+      </div>
       {posts.map((post) => {
         return (
           <p>
